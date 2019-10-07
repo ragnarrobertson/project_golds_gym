f58e2c135643a22167511352b9de7e8a44e7c538
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-10-07 14:28:38 +0100

    Added the bookings controller and have given it the restful ways for retrieving and reading the booking info

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,1 +1,35 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative( '../models/booking.rb' )
+also_reload( '../models.*' )
 
+get '/bookings' do
+  @bookings = Booking.all()
+  erb ( :"bookings/index")
+end
+
+# get '/bookings/new' do
+#   erb ( :"bookings/new" )
+# end
+#
+# post '/bookings' do
+#   @booking = Booking.new(params)
+#   @booking.save()
+#   erb ( :"bookings/create")
+# end
+#
+get '/bookings/:id' do
+  @booking = Booking.find(params['id'].to_i)
+  erb( :"bookings/show")
+end
+#
+# get "/bookings/:id/edit" do
+#   @booking = Booking.find( params[:id] )
+#   erb( :"bookings/edit" )
+# end
+#
+# post "/bookings/:id/delete" do
+#   booking = Booking.find( params[:id] )
+#   booking.delete()
+#   redirect to '/bookings'
+# end
--- golds_gym.rb
+++ golds_gym.rb
@@ -2,6 +2,7 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative("./controllers/activities_controller")
 require_relative("./controllers/members_controller")
+require_relative("./controllers/bookings_controller")
 
 get '/' do
   erb( :index )
